b006af9cdee87c07d121d424fdd0f06ef20a022e
Samuel Santos <samuka0102128@gmail.com>
2023-03-16 08:23:03 -0300

    Fazendo as rotas para a pagina de Login!

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { LoginForm } from './LoginForm';
+import { LoginCreate } from './LoginCreate';
+import { LoginPasswordLost } from './LoginPasswordLost';
+import { LoginPasswordReset } from './LoginPasswordReset';
 
 export const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />}></Route>
+        <Route path="criar" element={<LoginCreate />}></Route>
+        <Route path="perdeu" element={<LoginPasswordLost />}></Route>
+        <Route path="resetar" element={<LoginPasswordReset />}></Route>
+      </Routes>
+    </div>
+  );
 };
