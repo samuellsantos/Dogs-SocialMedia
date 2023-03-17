9f2a5ead1ec5bff8e79ec973adcc9fd9adfba80b
Samuel Santos <samuka0102128@gmail.com>
2023-03-17 09:01:17 -0300

    Estilizando a pagina de login e adicionando botoes para 'esqueceu a senha' e 'cadastro'

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -43,7 +43,7 @@ export const UserStorage = ({ children }) => {
       const { token } = await tokenRes.json();
       window.localStorage.setItem('token', token);
       await getUser(token);
-      /* navigate('/conta'); */
+      navigate('/conta');
     } catch (err) {
       setError(err.message);
       setLogin(false);
