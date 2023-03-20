e92ecf3367f7d19f001df5f586b78908726f764f
Samuel Santos <samuka0102128@gmail.com>
2023-03-20 19:31:24 -0300

    Foto Post

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -6,6 +6,10 @@ const types = {
       /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
     message: 'Preencha um email válido',
   },
+  number: {
+    regex: /^\d*$/,
+    message: 'Utilize apenas números.',
+  },
 };
 
 export const useForm = (type) => {
