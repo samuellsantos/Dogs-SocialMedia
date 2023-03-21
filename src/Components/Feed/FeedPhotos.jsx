63637270a43e87b0892dc0b876c9b6b1b84cf76e
Samuel Santos <samuka0102128@gmail.com>
2023-03-21 09:59:58 -0300

    Feed Modal

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -6,7 +6,7 @@ import { Error } from '../../Components/Helper/Error';
 import { Loading } from '../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
-export const FeedPhotos = () => {
+export const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
@@ -25,7 +25,11 @@ export const FeedPhotos = () => {
       <div>
         <ul className={`${styles.feed} animeLeft`}>
           {data.map((photo) => (
-            <FeedPhotosItem key={photo.id} photo={photo} />
+            <FeedPhotosItem
+              key={photo.id}
+              photo={photo}
+              setModalPhoto={setModalPhoto}
+            />
           ))}
         </ul>
       </div>
