83695ea372b5149a86519b2017ad7f91215b3adf
Samuel Santos <samuka0102128@gmail.com>
2023-03-22 20:57:09 -0300

    Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import footer from '../../Assets/dogs-footer.svg';
+
 export const Footer = () => {
-  return <div className={styles.footer}></div>;
+  return (
+    <footer className={styles.footer}>
+      <img src={footer} />
+      <p>Dogs. Alguns direitos reservados</p>
+    </footer>
+  );
 };
